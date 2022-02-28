6271942fa69330a7903b95a082f22800a6db72c1
Benedictus Nigel <99152688+Nigelap@users.noreply.github.com>
2022-02-28 10:31:58 +0700

    Update Insertionsort.c

--- Tugas ASD Sorting/Insertionsort.c
+++ Tugas ASD Sorting/Insertionsort.c
@@ -1,26 +1,42 @@
 #include <stdio.h>
-int main(){
-  int n, array[100], i, j, tmp;
-  printf("Masukkan Jumlah Elemen: ");
-  scanf("%d", &n);
-  printf("Masukkan nilai\n", n);
-  for(i = 0; i < n; i++){
-    scanf("%d", &array[i]);
-  }
-  for (i = 1; i <= n; i++){
-    j = i;
-    while(j > 0 && array[j-1] > array[j]){
-      tmp = array[j];
-      array[j] = array[j-1];
-      array[j-1] = tmp;
-      j--;
-    }
-  }
-  printf("Hasil pengurutan sebagai berikut:\n");
-  for (i = 0; i <= n-1; i++){
-    printf("%d ", array[i]);
-  }
-  printf("\n");
+#include <stdlib.h>
+//Insertion Sort
+void insertionSort(int *Data, int n){
+    int temp, j;
 
-  return 0;
+    for (int i = 1; i <= n-1; i++){
+        temp = Data[i];
+        j = i-1;
+
+        while(j>=0 && Data[j] > temp){
+            Data[j+1] = Data[j];
+            j--;
+        }
+        Data[j+1] = temp;
+    }
 }
+
+void print(int* Data, int n){
+    for (int i = 0; i < n; i++){
+        printf("%d ", Data[i]);
+    }
+    printf("\n");
+}
+
+int main(){
+
+    int Data[] = {23, 67, 89, 5, 45, 20, 15, 27};
+
+
+	int n = sizeof(Data)/sizeof(Data[0]);
+
+    printf("Sebelum : ");
+    print(Data, n);
+    printf("\n");
+
+    printf("Sesudah : ");
+    insertionSort(Data, n);
+    print(Data, n);
+    return 0;
+}
+
